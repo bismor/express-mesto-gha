628da568c51b668003edcad1146645b112c4e127
Vladislav Kosyachenko <cwat2011@yandex.ru>
2023-04-19 15:52:16 +0300

    error 404

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
+const HTTP_STATUS_CODE = require('./utils/http-status-code');
 
 const app = express();
 app.use(bodyParser.json());
@@ -23,5 +24,8 @@ app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res) => {
+  res.status(HTTP_STATUS_CODE.NOT_FOUND).send({ message: 'Not Found' });
+});
 
 app.listen(3000);
