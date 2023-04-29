d1e065e59f1b135ccdf5bf913028fd3fd79a674a
Vladislav Kosyachenko <cwat2011@yandex.ru>
2023-04-29 17:49:46 +0300

    роут для логина и регистрации

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const {
-  getUsers, createUser, getUserById, updateProfile, updateAvatar,
+  getUsers, createUser, getUserById, updateProfile, updateAvatar, login,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
 router.get('/:userId', getUserById);
-router.post('/', createUser);
 router.patch('/me', updateProfile);
 router.patch('/me/avatar', updateAvatar);
+router.post('/signin', login);
+router.post('/signup', createUser);
 
 module.exports = router;
