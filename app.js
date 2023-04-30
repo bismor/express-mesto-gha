e34d047761bf09f65ffbc69f33589d270d078bdc
Vladislav Kosyachenko <cwat2011@yandex.ru>
2023-04-30 20:12:41 +0300

    неопределенная ошибка

--- app.js
+++ app.js
@@ -18,7 +18,15 @@ app.use('/cards', auth, require('./routes/cards'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use((err, req, res, next) => {
-  res.status(err.statusCode).send({ message: err.message });
+  const { statusCode = 500, message } = err;
+  res
+    .status(statusCode)
+    .send({
+      // проверяем статус и выставляем сообщение в зависимости от него
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
 });
 
 app.listen(3000);
