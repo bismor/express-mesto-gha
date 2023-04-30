062b97c2e42e3d9d7e1df4821f72b8b927b6f1df
Vladislav Kosyachenko <cwat2011@yandex.ru>
2023-04-30 23:56:53 +0300

    лишний auth

--- routes/cards.js
+++ routes/cards.js
@@ -1,28 +1,27 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
-const auth = require('../middlewares/auth');
 const {
   getCards, createCard, deleteCardById, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
-router.get('/', auth, getCards);
-router.post('/', auth, celebrate({
+router.get('/', getCards);
+router.post('/', celebrate({
   body: Joi.object().keys({
     link: Joi.string().required(),
     name: Joi.string().required().min(2).max(30),
   }),
 }), createCard);
-router.delete('/:cardId', auth, celebrate({
+router.delete('/:cardId', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().required().regex(/^[0-9a-fA-F]{24}$/),
   }),
 }), deleteCardById);
-router.put('/:cardId/likes', auth, celebrate({
+router.put('/:cardId/likes', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().required().regex(/^[0-9a-fA-F]{24}$/),
   }),
 }), likeCard);
-router.delete('/:cardId/likes', auth, celebrate({
+router.delete('/:cardId/likes', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().required().regex(/^[0-9a-fA-F]{24}$/),
   }),
