00006f03c5a3755dd57cf3ac9450e0c2b663dd12
Vladislav Kosyachenko <cwat2011@yandex.ru>
2023-05-01 18:51:04 +0300

    joi avatar

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    avatar: Joi.string().required().regex(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/),
     email: Joi.string().required().regex(/^\S+@\S+\.\S+$/),
     password: Joi.string().required(),
   }),
